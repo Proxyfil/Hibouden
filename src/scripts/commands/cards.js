d826e8d62f0d09a7aec2b4bcd47d070e34e091db
Proxyfil <piloupilou200@gmail.com>
2022-05-16 16:03:46 +0200

    fix : V1.5k | Allow roll preview with embed

--- src/scripts/commands/cards.js
+++ src/scripts/commands/cards.js
@@ -116,7 +116,7 @@ module.exports = {
 
         //End Building of Action Row
         interaction.editReply({ embeds: [embed], files: [attachment], components: [row], ephemeral: true});
-        interaction.followUp({ content: "Roll de "+interaction.member.user.username, files: [attachment]})
+        interaction.followUp({ files: [attachment]})
     },
     card_select: function(interaction,choice){
 
