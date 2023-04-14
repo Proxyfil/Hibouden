83595b79488f0f0e842d936f7e012d94e475d892
Proxyfil <piloupilou200@gmail.com>
2023-04-14 21:14:39 +0200

    feat : updated website

--- website/vue.config.js
+++ website/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/Hibouden/' : '/'
+  ? '/' : '/Projets 2022/InGDoc/Hibouden/website/dist/'
 })
